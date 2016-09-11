a45b60a8ea4584b943f5461369297dbfd6a68e71
DESKTOP-C5KNARJ\mh122354 <michaelhabib1868@gmail.com>
2016-09-11 16:29:26 -0700

    Add configurations for routing pages

--- js/7MinWorkout/app.js
+++ js/7MinWorkout/app.js
@@ -1,5 +1,20 @@
 /**
  * Created by mh122354 on 9/11/2016.
  */
-angular.module('app',[]);
+angular.module('app',['ngRoute','7MinWorkout']).
+    config(function($routeProvider){
+        $routeProvider.when('/start',{
+            templateUrl:'partials/start.html'
+        });
+        $routeProvider.when('/workout',{
+            templateUrl:'partials/workout.html',
+            controller:'WorkoutController'
+        });
+        $routeProvider.when('/finish',{
+            templateUrl:'partials/finish.html'
+        });
+        $routeProvider.otherwise({
+            redirectTo:'/start'
+        });
+});
 angular.module('7MinWorkout',[]);
